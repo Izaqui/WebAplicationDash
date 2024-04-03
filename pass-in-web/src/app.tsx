a8a0b011015e69fa613046e20e549a90d554fbad
Izaqui <izaquiel.silva.ads@gmail.com>
2024-04-03 09:43:37 -0300

    projeto da NLWUnite concluido com sucesso

---
+++ pass-in-web/src/app.tsx
@@ -0,0 +1,12 @@
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+function App() {
+  return (
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-3 bg-slate-800">
+      <Header />
+      <AttendeeList />
+    </div>
+  )
+}
+
+export default App
